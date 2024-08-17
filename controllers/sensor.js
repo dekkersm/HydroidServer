ecaad0c04c77685acb9ac86228429c1d5637c037
idekkers <idekkers@hotmail.com>
2024-08-17 17:43:27 +0300

    data rearrange

--- controllers/sensor.js
+++ controllers/sensor.js
@@ -1,67 +1,80 @@
-const express = require('express');
-
-exports.getPhCurrent = (req, res, next) => {
-    res.json({
+const phdata = [
+    {
+        "id": 6,
         "ph": "7",
         "date": "1723901464564"
-    })
+    }, {
+        "id": 5,
+        "ph": "7.2",
+        "date": "1723901364564"
+    }, {
+        "id": 4,
+        "ph": "7.1",
+        "date": "1723901264564"
+    }, {
+        "id": 3,
+        "ph": "7.6",
+        "date": "1723901164564"
+    }, {
+        "id": 2,
+        "ph": "7.8",
+        "date": "1723901064564"
+    }, {
+        "id": 1,
+        "ph": "6.5",
+        "date": "1723900964564"
+    }
+]
+
+const tdsData = [
+    {
+        "id": 6,
+        "ph": "1252",
+        "date": "1723901464564"
+    }, {
+        "id": 5,
+        "ph": "1325",
+        "date": "1723901364564"
+    }, {
+        "id": 4,
+        "ph": "1245",
+        "date": "1723901264564"
+    }, {
+        "id": 3,
+        "ph": "1365",
+        "date": "1723901164564"
+    }, {
+        "id": 2,
+        "ph": "998",
+        "date": "1723901064564"
+    }, {
+        "id": 1,
+        "ph": "1023",
+        "date": "1723900964564"
+    }
+]
+
+
+exports.getPhCurrent = (req, res, next) => {
+    res.json(
+        phdata.reduce((a,b)=>a.id>b.id?a:b)
+    )
 };
 
 exports.getPhHistory = (req, res, next) => {
     res.json({
-        "phData": [
-            {
-                "ph": "7",
-                "date": "1723901464564"
-            }, {
-                "ph": "7.2",
-                "date": "1723901364564"
-            }, {
-                "ph": "7.1",
-                "date": "1723901264564"
-            }, {
-                "ph": "7.6",
-                "date": "1723901164564"
-            }, {
-                "ph": "7.8",
-                "date": "1723901064564"
-            }, {
-                "ph": "6.5",
-                "date": "1723900964564"
-            }
-        ]
+        "phData": phdata
     })
 }
 
 exports.getTdsCurrent = (req, res, next) => {
-    res.json({
-        "ph": "1252",
-        "date": "1723901464564"
-    })
+    res.json(
+        tdsData.reduce((a,b)=>a.id>b.id?a:b)
+    )
 };
 
 exports.getTdsHistory = (req, res, next) => {
     res.json({
-        "phData": [
-            {
-                "ph": "1252",
-                "date": "1723901464564"
-            }, {
-                "ph": "1325",
-                "date": "1723901364564"
-            }, {
-                "ph": "1245",
-                "date": "1723901264564"
-            }, {
-                "ph": "1365",
-                "date": "1723901164564"
-            }, {
-                "ph": "998",
-                "date": "1723901064564"
-            }, {
-                "ph": "1023",
-                "date": "1723900964564"
-            }
-        ]
+        "tdsData": tdsData
     })
 }
